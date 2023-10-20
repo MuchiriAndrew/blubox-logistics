a6ec8229a698aac7fc5465b788161bc632e3fd32
MuchiriAndrew <kariukia225@gmail.com>
2023-10-20 19:21:18 +0300

    update

---
+++ src/components/Tracking.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+
+function Tracking() {
+  return (
+    <>
+    <div style={{width: '1316px', height: '1690px', position: 'absolute', top:"1939px", left:"68px"}}>
+      <div style={{width: 992, left: 162, top: 0, position: 'absolute', color: '#292D32', fontSize: 40, fontFamily: 'Quicksand', fontWeight: '700', wordWrap: 'break-word'}}>Manage your shipment from anywhere in the world</div>
+
+      <div style={{width: 471, height: 953.56, left: 0, top: 154, position: 'absolute'}}>
+      <img style={{width: 471, height: 953.56, left: 0, top: 0, position: 'absolute'}} src="./assets/outerphone.png" />
+      <img style={{width: 424.77, height: 915.99, left: 23.12, top: 17.34, position: 'absolute'}} src="./assets/iPhone.png" />
+      </div>
+
+      <div className='' style={{width: 302, height: 321, left: 252, top: 428, position: 'absolute'}}>
+      <div style={{width: 409, height: 224, left: -19, top: -93, position: 'absolute'}}>
+
+      <img style={{width: 409, height: 224, left: 0, top: 0, position: 'absolute', borderRadius: 20, border: '1px #7990FB solid'}} src="../assets/map.png" />
+
+      <svg style={{position:'absolute', top:"97px", left:"263px"}} width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="20.9244" cy="20.9244" r="20.9244" fill="#7990FB" fill-opacity="0.2"/>
+      </svg>
+
+      <svg style={{position:'absolute', top:"103px", left:"268px", zIndex:"999"}} width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="15" cy="15" r="13.3307" fill="white" stroke="#D9D9D9" stroke-width="3.33864"/>
+      <circle cx="14.9998" cy="15.0001" r="3.46154" fill="#7990FB"/>
+      </svg>
+
+      <img style={{position:"absolute", top:"159.08px", left:"231px"}} src="../assets/photo.svg" alt="" />
+
+      <svg style = {{position:'absolute', top:"123.5px", left:"249.83px"}} width="39" height="46" viewBox="0 0 39 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M2.83194 43.3067L12.9454 37.0294L2.83194 11.9201L27.9412 8.08398L33.8698 4.24785L37.7059 2.50415" stroke="#B8B8B8" stroke-width="4.45152" stroke-linejoin="round" stroke-dasharray="4.45 4.45"/>
+      </svg>
+
+      <svg style = {{position:'absolute', top:"51px", left:"32px"}} width="263" height="153" viewBox="0 0 263 153" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M253.876 58.4856V52.1725L260 41.5616L250.592 27.0687L256.579 10.2465L211.679 3L152.667 16.1989L96.3563 29.6567L88.9517 31.2095L79.9717 33.2799L63.044 37.6796L48.3278 41.5616L36.782 43.8908L54.3145 69.0644L3 83.7465L33.3611 125.155L42.3411 136.283L48.3278 143.271L52.1764 150L94.9384 135.766L93.6556 128.261" stroke="url(#paint0_linear_1_122)" stroke-width="4.5" stroke-linecap="round" stroke-linejoin="round"/>
+      <defs>
+      <linearGradient id="paint0_linear_1_122" x1="92.5492" y1="157.851" x2="111.437" y2="20.2339" gradientUnits="userSpaceOnUse">
+      <stop stop-color="#7990FB"/>
+      <stop offset="1" stop-color="#7990FB" stop-opacity="0.17"/>
+      </linearGradient>
+      </defs>
+      </svg>
+
+      </div>
+      </div>
+
+
+
+      <div style={{width: 376, left: 689, top: 262, position: 'absolute', color: '#292D32', fontSize: 40, fontFamily: 'Quicksand', fontWeight: '700', wordWrap: 'break-word'}}>You can track your shipment real time</div>
+      <div style={{width: 505, left: 689, top: 392, position: 'absolute', color: '#292D32', fontSize: 16, fontFamily: 'Quicksand', fontWeight: '400', wordWrap: 'break-word'}}>You can track your shipment real time by making use of our mobile application for easy and seamless tracking anywhere, anytime.</div>
+      <div style={{paddingLeft: 30, paddingRight: 30, paddingTop: 16, paddingBottom: 16, left: 689, top: 478, position: 'absolute', background: '#7990FB', boxShadow: '0px 2px 20px rgba(121, 144, 251, 0.25)', borderRadius: 50, border: '1px #848586 solid', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 20, display: 'inline-flex'}}>
+      <div style={{textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Quicksand', fontWeight: '500', wordWrap: 'break-word'}}>Get started</div>
+      </div>
+
+
+      <div style={{width: 697.86, height: 465, left: 566, top: 901, position: 'absolute'}}>
+      <img style={{width: 697.86, height: 465, left: 0, top: 0, position: 'absolute'}} src="../assets/outeripad.png" />
+      <img style={{width: 655.07, height: 420.60, left: 20.68, top: 16.05, position: 'absolute'}} src="../assets/iPad.png" />
+      </div>
+      <div style={{width: 544, left: 566, top: 1396, position: 'absolute', color: '#292D32', fontSize: 40, fontFamily: 'Quicksand', fontWeight: '700', wordWrap: 'break-word'}}>Manage your shipments seamlessly and effortlessly</div>
+      <div style={{width: 505, left: 566, top: 1526, position: 'absolute', color: '#292D32', fontSize: 16, fontFamily: 'Quicksand', fontWeight: '400', wordWrap: 'break-word'}}>You can now view, manage and track your shipments seamlessly and effortlessly through your dashboard</div>
+      <div style={{paddingLeft: 30, paddingRight: 30, paddingTop: 16, paddingBottom: 16, left: 566, top: 1612, position: 'absolute', background: '#7990FB', boxShadow: '0px 2px 20px rgba(121, 144, 251, 0.25)', borderRadius: 50, border: '1px #848586 solid', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 20, display: 'inline-flex'}}>
+      <div style={{textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Quicksand', fontWeight: '500', wordWrap: 'break-word'}}>Get started</div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Tracking
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Services from '../components/Services'
+import Tracking from '../components/Tracking'
 import '../styles/Home.css'
 
 function Home() {
@@ -11,6 +12,7 @@ function Home() {
     <Navbar/>
     <Header/>
     <Services/>
+    <Tracking />
     {/* </div> */}
     </>
   )
