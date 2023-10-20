3d243971dbcf2fbd567135d41ce50f1ef6b151c1
MuchiriAndrew <kariukia225@gmail.com>
2023-10-20 20:37:02 +0300

    update

---
+++ src/components/WhyChoose.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import '../styles/WhyChoose.css'
+
+function WhyChoose() {
+  return (
+    <>
+    <div id='mine' style={{width: '1443px', height: '821px', position: 'absolute', top:"3735px", left:"2px"}}>
+    <img style={{width: 545, height: 556, left: 839, top: 144, position: 'absolute', borderRadius: 10}} src="../assets/staff.png" />
+        <svg style={{position:"absolute", top:"47px", left:"757px"}} width="118" height="118" viewBox="0 0 118 118" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M86.5694 68.5258L60.1967 88.1953C59.0183 89.0742 57.0092 89.1318 55.7794 88.3025L19.933 64.4766C14.8041 61.0539 15.6728 58.1792 21.7911 58.0696L38.571 57.7364C41.3363 57.6388 42.9312 55.4451 42.1892 52.8111L37.3132 35.97C35.7456 30.5898 38.3548 28.6437 43.0647 31.6803L57.8164 41.1558C60.1295 42.618 62.687 41.7148 63.5409 39.1131L68.348 24.1319C70.3811 17.7253 73.678 17.7551 75.6699 24.1725L88.0015 64.3576C88.3758 65.7376 87.7477 67.6469 86.5694 68.5258Z" stroke="#FDBB76" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M64.2716 98.3423L95.1332 75.3247" stroke="#FDBB76" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M55.9484 69.6199L69.9764 59.1573" stroke="#FDBB76" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+    <div style={{height: 276, left: 75, top: 124, position: 'absolute'}}>
+        <div style={{width: 436, left: 0, top: 0, position: 'absolute', color: '#292D32', fontSize: 40, fontFamily: 'Quicksand', fontWeight: '700', wordWrap: 'break-word'}}>Why BluBox Logistics?</div>
+        <div style={{width: 641, left: 0, top: 80, position: 'absolute', color: '#292D32', fontSize: 20, fontFamily: 'Quicksand', fontWeight: '400', wordWrap: 'break-word'}}>BluBox Logistics has been providing shipping services for more than 10 years with our branches spread around in more than 5 countries including united states of America and has delivered a total number of 1Million shipments. We didn’t just get here, we started small, we made mistakes, we fell on our faces, we learnt from our mistakes, and here we are. The tiny little stars in this section represents our branches in the world’s map</div>
+    </div>
+
+
+    <div style={{paddingLeft: 30, paddingRight: 30, paddingTop: 16, paddingBottom: 16, left: 75, top: 456, position: 'absolute', background: '#7990FB', boxShadow: '0px 2px 20px rgba(121, 144, 251, 0.25)', borderRadius: 50, border: '1px #848586 solid', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 20, display: 'inline-flex'}}>
+
+        <div style={{textAlign: 'center', color: 'white', fontSize: 20, fontFamily: 'Quicksand', fontWeight: '500', wordWrap: 'break-word'}}>Learn More</div>
+
+        <div style={{width: 24, height: 30, justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M14.43 5.93018L20.5 12.0002L14.43 18.0702" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M3.5 12H20.33" stroke="white" stroke-width="1.5" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+
+        </div>
+    </div>
+
+    <svg style={{position:"absolute", top:"544", left:"320"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"592", left:"387"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"696", left:"310"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"60", left:"229"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"440", left:"767"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"554", left:"737"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"79", left:"941"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"27", left:"1014"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"69.1", left:"1155.24"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+    <svg style={{position:"absolute", top:"380.91", left:"610.3"}} width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.97525 0.179282L12.4418 7.24714L19.4128 9.97542L12.345 12.442L9.61669 19.413L7.15013 12.3451L0.179117 9.61686L7.24697 7.15029L9.97525 0.179282Z" fill="#FDBB76"/>
+    </svg>
+
+
+    </div>
+    </>
+  )
+}
+
+export default WhyChoose
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,16 +3,18 @@ import Navbar from '../components/Navbar'
 import Header from '../components/Header'
 import Services from '../components/Services'
 import Tracking from '../components/Tracking'
+import WhyChoose from '../components/WhyChoose'
 import '../styles/Home.css'
 
 function Home() {
   return (
     <>
     {/* <div id='wrapper'> */}
-    <Navbar/>
-    <Header/>
-    <Services/>
-    <Tracking />
+      <Navbar/>
+      <Header/>
+      <Services/>
+      <Tracking />
+      <WhyChoose />
     {/* </div> */}
     </>
   )
