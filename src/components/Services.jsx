1f2d568fd8b3d85ff27370a45defbb0bb3075443
MuchiriAndrew <kariukia225@gmail.com>
2023-11-03 18:56:54 +0300

    services styling

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import '../styles/Services.css'
 
 function Services() {
   return (
     <>
-    <div className='' style={{width: '1316px', height: '718px', position:"absolute", top:"1026px", left:"68px"}}>
+    <div id='wrapper3'>
 
-    <div style={{width: 488, left: 0, top: 300, position: 'absolute', color: '#292D32', fontSize: 20, fontFamily: 'Satoshi', fontWeight: '400', wordWrap: 'break-word'}}>Find out below what we can do to make your logistics easy and seamless.</div>
-    <div style={{width: 441, left: 0, top: 220, position: 'absolute', color: '#292D32', fontSize: 40, fontFamily: 'Quicksand', fontWeight: '700', wordWrap: 'break-word'}}>Our Awesome Services</div>
+    <div id='services-paragraph' >Find out below what we can do to make your logistics easy and seamless.</div>
 
-    <div style={{width: 426, height: 266, left: 0, top: 452, position: 'absolute', background: '#F7F7F7', borderRadius: 40, border: '1px #FFAB53 solid'}}/>
-    <div style={{width: 426, height: 266, left: 445, top: 452, position: 'absolute', background: '#F7F7F7', borderRadius: 40, border: '1px #B9C5FF solid'}} />
-    <div style={{width: 426, height: 266, left: 890, top: 452, position: 'absolute', background: '#F7F7F7', borderRadius: 40, border: '1px #FFD1FA solid'}} />
+    <div id='services-heading' >Our Awesome Services</div>
 
-    <div style={{width: 54, height: 54, left: 26, top: 474, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+    <div id='custom-clearance-box'/>
+    <div id='real-time-tracking-box'/>
+    <div id='cargo-insurance-box'/>
+
+    <div id='svg-div1'>
       <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M49.5 22.4999V33.7499C49.5 44.9999 45 49.4999 33.75 49.4999H20.25C9 49.4999 4.5 44.9999 4.5 33.7499V20.2499C4.5 8.99988 9 4.49988 20.25 4.49988H31.5" stroke="#FFAB53" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       <path d="M49.5 22.4999H40.5C33.75 22.4999 31.5 20.2499 31.5 13.4999V4.49988L49.5 22.4999Z" stroke="#FFAB53" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -21,14 +23,14 @@ function Services() {
       </svg>
     </div>
 
-    <div style={{width: 54, height: 54, left: 471, top: 474, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+    <div id='svg-div2'>
         <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M27 30.2174C30.8771 30.2174 34.02 27.0744 34.02 23.1974C34.02 19.3203 30.8771 16.1774 27 16.1774C23.123 16.1774 19.98 19.3203 19.98 23.1974C19.98 27.0744 23.123 30.2174 27 30.2174Z" stroke="#B9C5FF" stroke-width="2"/>
         <path d="M8.14499 19.1024C12.5775 -0.382617 41.445 -0.360116 45.855 19.1249C48.4425 30.5549 41.3325 40.2299 35.1 46.2149C30.5775 50.5799 23.4225 50.5799 18.8775 46.2149C12.6675 40.2299 5.55749 30.5324 8.14499 19.1024Z" stroke="#B9C5FF" stroke-width="2"/>
         </svg>
     </div>
 
-    <div style={{width: 54, height: 54, left: 916, top: 474, position: 'absolute', justifyContent: 'center', alignItems: 'center', display: 'inline-flex'}}>
+    <div id='svg-div3'>
       <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M47.0475 25.0198C47.0475 36.0223 39.06 46.3273 28.1475 49.3423C27.405 49.5448 26.595 49.5448 25.8525 49.3423C14.94 46.3273 6.95251 36.0223 6.95251 25.0198V15.1423C6.95251 13.2973 8.34755 11.2048 10.08 10.5073L22.6125 5.37735C25.425 4.22985 28.5975 4.22985 31.41 5.37735L43.9425 10.5073C45.6525 11.2048 47.0701 13.2973 47.0701 15.1423L47.0475 25.0198Z" stroke="#FFD1FA" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
       <path d="M27 28.1249C29.4853 28.1249 31.5 26.1102 31.5 23.6249C31.5 21.1396 29.4853 19.1249 27 19.1249C24.5147 19.1249 22.5 21.1396 22.5 23.6249C22.5 26.1102 24.5147 28.1249 27 28.1249Z" stroke="#FFD1FA" stroke-width="2" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
@@ -36,24 +38,24 @@ function Services() {
       </svg>
     </div>
 
-    <div style={{left: 26, top: 555, position: 'absolute', color: '#292D32', fontSize: 24, fontFamily: 'Quicksand', fontWeight: '700', wordWrap: 'break-word'}}>Custom clearance</div>
-    <div style={{left: 471, top: 555, position: 'absolute', color: '#292D32', fontSize: 24, fontFamily: 'Quicksand', fontWeight: '700', wordWrap: 'break-word'}}>Real time tracking</div>
-    <div style={{left: 916, top: 555, position: 'absolute', color: '#292D32', fontSize: 24, fontFamily: 'Quicksand', fontWeight: '700', wordWrap: 'break-word'}}>Cargo insurance</div>
+    <div id='custom-clearance-heading'>Custom clearance</div>
+    <div id='real-time-tracking-heading'>Real time tracking</div>
+    <div id='cargo-insurance-heading'>Cargo insurance</div>
 
-    <div style={{width: 387, left: 26, top: 602, position: 'absolute', color: '#292D32', fontSize: 16, fontFamily: 'Quicksand', fontWeight: '400', wordWrap: 'break-word'}}>We provide you with all the necessary documents required to get a fast, clean, swift and seamless custom clearance.</div>
-    <div style={{width: 387, left: 471, top: 602, position: 'absolute', color: '#292D32', fontSize: 16, fontFamily: 'Quicksand', fontWeight: '400', wordWrap: 'break-word'}}>Watch your shipment make its way to you. we provide you with real time tracking so you can know where your shipment has been to.</div>
-    <div style={{width: 357, left: 916, top: 602, position: 'absolute', color: '#292D32', fontSize: 16, fontFamily: 'Quicksand', fontWeight: '400', wordWrap: 'break-word'}}> Do not panic about loosing your shipment, we have got you covered with our insurance policy.</div>
+    <div id='custom-clearance-description'>We provide you with all the necessary documents required to get a fast, clean, swift and seamless custom clearance.</div>
+    <div id='real-time-tracking-description' >Watch your shipment make its way to you. we provide you with real time tracking so you can know where your shipment has been to.</div>
+    <div id='cargo-insurance-description'> Do not panic about loosing your shipment, we have got you covered with our insurance policy.</div>
 
-      <svg style={{position:"absolute",top:"207.1px", left:"503px"}} width="51" height="84" viewBox="0 0 51 84" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg id='stars' width="51" height="84" viewBox="0 0 51 84" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M25.6061 23.9715L31.8746 41.9336L49.5905 48.8672L31.6284 55.1357L24.6948 72.8516L18.4264 54.8895L0.710394 47.9559L18.6725 41.6875L25.6061 23.9715Z" fill="#7990FB"/>
       <path d="M18.2197 9.28304L20.6863 16.3509L27.6573 19.0792L20.5895 21.5457L17.8612 28.5168L15.3946 21.4489L8.42359 18.7206L15.4914 16.2541L18.2197 9.28304Z" fill="#FDBB76"/>
       </svg>
 
-      <svg style={{position:"absolute",top:"117.99px", left:"537.18px"}} width="472" height="275" viewBox="0 0 473 245" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg id='line' width="472" height="275" viewBox="0 0 473 245" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M1.72286 122.998C51.2217 129.565 99.3488 132.458 145.653 121.991C195.538 110.715 245.51 89.4627 269.861 58.156C284.558 39.2606 285.252 20.8977 250.782 8.44734C222.507 -1.76542 205.939 -0.900814 191.457 17.8329C175.583 38.3674 198.237 62.3186 216.833 81.4105C241.315 106.547 288.334 122.752 327.037 134.764C357.117 144.099 385.665 150.359 412.025 164.579C429.778 174.156 445.954 186.594 458.199 199.341C472.065 213.778 471.556 227.598 470.64 243.03" stroke="#101828" stroke-width="3" stroke-linecap="round"/>
       </svg>
 
-      <svg style={{position:"absolute",top:"360.75px", left:"993.12px"}} width="21" height="24" viewBox="0 0 21 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg id='arrow' width="21" height="24" viewBox="0 0 21 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M11.0673 21.93C9.07845 24.6844 4.73282 23.6198 3.97705 20.193L0.694756 5.31042C0.0601086 2.43281 2.52565 -0.0386316 5.34257 0.65148L17.2625 3.57172C20.0794 4.26183 21.3751 7.65472 19.7049 9.96769L11.0673 21.93Z" fill="#101828"/>
       </svg>
 </div>
